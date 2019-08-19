60c39ff18b973ec8cb0021ec23b9ac8b5ef75bfc
Riad Mohamed <riaduk1@gmail.com>
2019-08-19 23:40:53 +0200

    Added basic classes to represent the blocks and the blocks array

---
+++ DesktopApp/Block.java
@@ -0,0 +1,20 @@
+public class Block {
+    int xPosition;
+    int yPosition;
+    String color;
+    int getXPosition() {
+        int xpos = 0;
+        // code to run the python scripts and get the x-pos.
+        return xpos;
+    }
+    int getYPosition() {
+        int ypos = 0;
+        // code to run the python scripts and get the y-pos.
+        return ypos;
+    }
+    int getColor() {
+        String color = "green";
+        return color;
+    }
+
+}
